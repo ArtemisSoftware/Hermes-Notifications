aebefb75c8642581c2e3139ea8a0ea7db9afae02
gustavo <gustavo.isaac@gmail.com>
2024-08-13 17:25:36 +0100

    NotificationConstants

---
+++ app/src/main/java/com/artemissoftware/hermesnotifications/NotificationConstants.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.hermesnotifications
+
+object NotificationConstants {
+
+    const val CHANNEL_ID = "water_reminder"
+    const val CHANNEL_NAME = "Water reminder channel"
+}
